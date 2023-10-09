cbdd326070cb03ed8fed45624504ba955f1e7e63
David Lai <laidav@gmail.com>
2023-10-08 20:55:17 -0400

    initiate vanilla project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hi')
+import { Hub, HubFactory } from "@hub-fx/core";
+import { tap } from "rxjs/operators";
+
+const hub = HubFactory();
+
+const reducer = (state = 3) => state;
+
+const store$ = hub.store({ reducer });
+
+store$
+  .pipe(tap((number) => console.log(number, "in tap")))
+  .subscribe((number) => {
+    console.log(number, "in subscribe");
+  });
