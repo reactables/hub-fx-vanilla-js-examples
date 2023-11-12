a575236e6b7af97f4fa02c825a2f33b771f9facd
David Lai <laidav@gmail.com>
2023-11-12 10:21:37 -0500

    consume counter

--- src/index.js
+++ src/index.js
@@ -1,35 +1,15 @@
-import { HubFactory } from '@hub-fx/core';
+import { Counter } from '@hub-fx/examples';
 
-// Actions
-const INCREMENT = 'INCREMENT';
-const increment = () => ({ type: INCREMENT });
+const {
+  state$,
+  actions: { increment, reset },
+} = Counter();
 
-const RESET = 'RESET';
-const reset = () => ({ type: RESET });
-
-// Reducer function to handle state updates
-const countReducer = (state = { count: 0 }, action) => {
-  switch (action?.type) {
-    case INCREMENT:
-      return { count: state.count + 1 };
-    case RESET:
-      return { count: 0 };
-    default:
-      return state;
-  }
-};
-
-// Initialize Hub
-const hub = HubFactory();
-
-// Initialize counter store
-const counterStore$ = hub.store({ reducer: countReducer });
-
-counterStore$.subscribe(({ count }) => {
+state$.subscribe(({ count }) => {
   // Update the count when state changes.
   document.getElementById('count').innerHTML = count;
 });
 
 // Bind click handlers
-document.getElementById('increment').addEventListener('click', () => hub.dispatch(increment()));
-document.getElementById('reset').addEventListener('click', () => hub.dispatch(reset()));
+document.getElementById('increment').addEventListener('click', increment);
+document.getElementById('reset').addEventListener('click', reset);
